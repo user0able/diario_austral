6789241a454c0533e368a602e5841d9945973e0e
Hern4n <hernanfontanez@gmail.com>
2025-03-13 00:35:13 -0300

    Update .gitignore and refactor hd.py; remove unused scripting.py and metadata PDFs

--- hd.py
+++ hd.py
@@ -12,7 +12,8 @@ ultima_pagina = 0
 
 
 def getFecha(
-        dias=200):  # cantidad de días hacia atrás que buscará el diario :3 / está en 0, porque lo buscará para hoy we :)
+    dias=200,
+):  # cantidad de días hacia atrás que buscará el diario :3 / está en 0, porque lo buscará para hoy we :)
     hoy = datetime.date.today()
     cantidad_dias_restar = datetime.timedelta(days=dias)
     dia_pasado = hoy - cantidad_dias_restar
@@ -25,22 +26,23 @@ def agrega_ceros_izquierda(numero):
 
 
 def genera_url_fecha(fecha=getFecha()):
-    url = requests.get("https://www.australvaldivia.cl/impresa/%s/%s/%s/full/cuerpo-principal/1/" %
-                       (
-                           fecha.year,
-                           agrega_ceros_izquierda(fecha.month),
-                           agrega_ceros_izquierda(fecha.day)
-                       )
-                       )
+    url = requests.get(
+        "https://www.australvaldivia.cl/impresa/%s/%s/%s/full/cuerpo-principal/1/"
+        % (
+            fecha.year,
+            agrega_ceros_izquierda(fecha.month),
+            agrega_ceros_izquierda(fecha.day),
+        )
+    )
     return url
 
 
 url = genera_url_fecha()
 
-soup = BeautifulSoup(url.content, 'html.parser')
+soup = BeautifulSoup(url.content, "html.parser")
 links = []
 soup.find_all("section")
-temp_link = ''
+temp_link = ""
 for link in soup.find_all("section"):
     temp_link = link.get("data-page")
     if temp_link:
@@ -51,8 +53,9 @@ for numero in range(1, 50):
     test_link = temp_link.replace("01.pdf", agrega_ceros_izquierda(numero) + ".pdf")
     if requests.get(test_link).status_code == 200:
         pdfs.append(test_link)  # añade el nombre del pdf a la lista "pdfs"
-        nombre_temporal_archivo = open('temp/metadata_%s' % test_link[-10:], 'wb').write(
-            requests.get(test_link).content)
+        nombre_temporal_archivo = open(
+            "temp/metadata_%s" % test_link[-10:], "wb"
+        ).write(requests.get(test_link).content)
     else:
         print("hasta la vista beibi")
         ultima_pagina = numero
@@ -62,13 +65,13 @@ print(pdfs)
 
 pdfs_locales = []
 for n in range(1, ultima_pagina):
-    pdfs_locales.append('temp/metadata_pag_%s.pdf' % agrega_ceros_izquierda(n))
+    pdfs_locales.append("temp/metadata_pag_%s.pdf" % agrega_ceros_izquierda(n))
 
 print(pdfs_locales)
 fusionador = PdfFileMerger()
 
 for pdf in pdfs_locales:
-    fusionador.append(open(pdf, 'rb'))
+    fusionador.append(open(pdf, "rb"))
 
 nombre_archivo_salida = "%s-%s-%s.pdf" % (
     agrega_ceros_izquierda(getFecha().year),
@@ -76,5 +79,5 @@ nombre_archivo_salida = "%s-%s-%s.pdf" % (
     agrega_ceros_izquierda(getFecha().day),
 )
 
-with open(nombre_archivo_salida, 'wb') as salida:
+with open(nombre_archivo_salida, "wb") as salida:
     fusionador.write(salida)
--- scripting.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = requests.get("https://www.australvaldivia.cl/impresa/2020/03/13/papel/")
-
-print(url.content)
